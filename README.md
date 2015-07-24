8a9ed06ba1201bf6723dcd5a397532af9db55fb1
Tim Smith <tim@cozy.co>
2015-07-23 18:41:42 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# sensu-plugins-ups
+## Sensu-Plugins-ups-checks
+[ ![Build Status](https://travis-ci.org/sensu-plugins/sensu-plugins-ups-checks.svg?branch=master)](https://travis-ci.org/sensu-plugins/sensu-plugins-ups-checks)
+[![Gem Version](https://badge.fury.io/rb/sensu-plugins-ups-checks.svg)](http://badge.fury.io/rb/sensu-plugins-ups-checks)
+[![Code Climate](https://codeclimate.com/github/sensu-plugins/sensu-plugins-ups-checks/badges/gpa.svg)](https://codeclimate.com/github/sensu-plugins/sensu-plugins-ups-checks)
+[![Test Coverage](https://codeclimate.com/github/sensu-plugins/sensu-plugins-ups-checks/badges/coverage.svg)](https://codeclimate.com/github/sensu-plugins/sensu-plugins-ups-checks)
+[![Dependency Status](https://gemnasium.com/sensu-plugins/sensu-plugins-ups-checks.svg)](https://gemnasium.com/sensu-plugins/sensu-plugins-ups-checks)
+## Functionality
+
+**metrics-nut**
+
+Polls metrics from NUT (http://www.networkupstools.org/) monitored UPS units
+
+## Files
+ * bin/metrics-nut.rb
+
+## Installation
+
+[Installation and Setup](http://sensu-plugins.io/docs/installation_instructions.html)
+
+## Notes
+
---
+++ bin/metrics-nut.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+#  encoding: UTF-8
+#   metrics-nut.rb
+#
+# DESCRIPTION:
+#   This plugin uses polls UPS metrics from NUT and produces
+#   Graphite formated output.
+#
+# OUTPUT:
+#   metric data
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   nut
+#
+# USAGE:
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2015 Tim Smith <tim@cozy.co> Cozy Services Ltd.
+#   Based on check-temperature, Copyright 2012 Wantudu SL <dsuarez@wantudu.com>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/metric/cli'
+require 'socket'
+
+class Nut < Sensu::Plugin::Metric::CLI::Graphite
+  option :scheme,
+         description: 'Metric naming scheme, text to prepend to .$parent.$child',
+         long: '--scheme SCHEME',
+         default: "#{Socket.gethostname}.ups"
+
+  def run
+    instances = `upsc -l`.split("\n")
+
+    metrics = {}
+    instances.each do |ups|
+      metrics["#{ups}"] = {}
+      output = `upsc #{ups}`.split("\n")
+      output.each do |line|
+        case line
+        when /battery.voltage: (.+)/
+          metrics["#{ups}"]['battery_voltage'] = $1
+        when /input.voltage: (.+)/
+          metrics["#{ups}"]['input_voltage'] = $1
+        when /ups.temperature: (.+)/
+          metrics["#{ups}"]['temperature'] = $1 unless $1 == '0' # 0 means it's not supported
+        when /battery.charge: (.+)/
+          metrics["#{ups}"]['battery_charge'] = $1
+        when /ups.load: (.+)/
+          metrics["#{ups}"]['load'] = $1
+        when /ups.status: (.+)/
+          if $1 == 'OL'
+            metrics["#{ups}"]['on_battery'] = 0
+          else
+            metrics["#{ups}"]['on_battery'] = 1
+          end
+        end
+      end
+    end
+
+    timestamp = Time.now.to_i
+
+    metrics.each do |ups, data|
+      data.each do |key, value|
+        output [config[:scheme], ups, key].join('.'), value, timestamp
+      end
+    end
+
+    ok
+  end
+end
